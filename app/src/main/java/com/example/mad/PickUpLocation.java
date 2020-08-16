a0418665cd4777945478fa376e604200dab2e09f
Catalystnick <seanwhitegns@gmail.com>
2020-08-17 01:14:40 +0530

    BookCarActivity, toyota image, pickup location

--- app/src/main/java/com/example/mad/PickUpLocation.java
+++ app/src/main/java/com/example/mad/PickUpLocation.java
@@ -1,8 +1,11 @@
 package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.WidgetContainer;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PickUpLocation extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class PickUpLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_up_location);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PickUpLocation.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Locations));
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
     }
 }
