38ace103a6c8b0c549f40033093969201c1b979a
Catalystnick <seanwhitegns@gmail.com>
2020-08-16 23:48:01 +0530

    BookCarActivity, toyota image

--- app/src/main/java/com/example/mad/CarRentalActivity.java
+++ app/src/main/java/com/example/mad/CarRentalActivity.java
@@ -2,13 +2,32 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CarRentalActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.car_rental);
+
+
+        button = (Button) findViewById(R.id.menubtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
